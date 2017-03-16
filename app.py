816522c96dd12939990ea836ee0328cf0f3dd7a9
mktia <endoplasmicreticulum2468741059@gmail.com>
2017-03-17 00:05:10 +0900

    fix

--- app.py
+++ app.py
@@ -5,10 +5,19 @@ import os
 import tweepy
 import urllib2
 
+###############################################
+consumer_key = 'osJDCMZaKvbTKbSiHniC2Uyrl'
+consumer_secret = 'T6ya6o7evcnc0DtETOjxmCvfJI6VgBhzXr0eLBlCy3lGsK2Mts'
+access_token = '1131457412-RbZrvbB00tvopPNES6Zrbha9bZ57zTyqcM8TlAy'
+access_token_secret = 'KoHMWNxxrwDLqQeoaGX40vJM3W6ZlgfXXRP3SEGFVTCJi'
+###############################################
+
+"""
 consumer_key = os.environ['ck']
 consumer_secret = os.environ['cs']
 access_token = os.environ['at']
 access_token_secret = os.environ['as']
+"""
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
@@ -17,6 +26,7 @@ feed_url_all = 'http://mikuta.main.jp/feed/data.html'
 
 def check_post(url, list_name):
 	res = feedparser.parse(url)
+	print(res)
 	title = res.entries[0].title
 	link = res.entries[0].link
 	list = api.get_list(owner_screen_name='_mktia', slug=list_name)
@@ -28,7 +38,6 @@ def check_post(url, list_name):
 		print('Posted.')
 	else:
 		print(list_name + ' is not updated.')
-	
 			
 if __name__ == '__main__':
 	#data = urllib2.urlopen(feed_url_all).read()
@@ -36,6 +45,6 @@ if __name__ == '__main__':
 	data = ast.literal_eval(data)
 	for i in range(len(data['site'])):
 		print(data['site'][i], data['list'][i])
-		check_post('http://' + data['site'][i], data['list'][i])
+		check_post('http://' + data['site'][i] + '/feed', data['list'][i])
 	
 	print('---------- finished! ----------')
